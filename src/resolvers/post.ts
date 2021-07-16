6772344b24f223336fd32f527819f5b856c2a422
Cody Siegmann <codysiegmann09@gmail.com>
2021-07-16 16:18:11 -0700

    1hr in

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,11 +1,19 @@
-import { Post } from "src/entities/Post";
-import { MyContext } from "src/types";
-import { Query, Resolver, Ctx } from "type-graphql";
+import { Post } from "../entities/Post";
+import { MyContext } from "../types";
+import { Query, Resolver, Ctx, Arg, Int } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id: id });
   }
 }
